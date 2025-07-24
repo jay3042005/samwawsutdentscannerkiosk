1efaaea9246adb1aa2d6c22f63dda18bdc322505
Arjay Dela Cruz <Jaymia3042005@gmail.com>
2025-07-24 12:50:13 +0800

    Update fix issue

--- LOgger/app/src/main/java/com/example/logger/data/model/ScheduleItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.logger.data.model
-
-data class ScheduleItem(
-    val id: String,
-    val title: String,
-    val date: String,
-    val time: String,
-    val location: String? = null
-)
